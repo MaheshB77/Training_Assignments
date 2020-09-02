31e8ca7e1ad463d8943eb1d85f454618f5435052
MaheshB77 <47536121+MaheshB77@users.noreply.github.com>
2020-09-02 12:38:23 +0530

    Changes in JavaScript assignment 1 (Added form data in an array and displayed it on the console)

--- javascript_assignments/assignment_1/javascript_assignment_1.js
+++ javascript_assignments/assignment_1/javascript_assignment_1.js
@@ -1,19 +1,25 @@
 // Showing all input fields from alert()
-function submitData(){
-    let data="";
-    let allInputs = document.getElementsByClassName("data");
-    let allInputsArray = Array.prototype.slice.call(allInputs);
-    allInputsArray.forEach(element => {
-        data+=element.value+" ";
-    });
-    alert(data);
+function submitData() {
+  let data = "";
+  let dataArray = [];
+  let allInputs = document.getElementsByClassName("data");
+  let allInputsArray = Array.prototype.slice.call(allInputs);
+  allInputsArray.forEach((element) => {
+    data += element.value + " ";
+    dataArray.push(element.value);
+  });
+  console.clear();
+
+  //   Showing the data array on console
+  console.log(dataArray);
+  alert(data);
 }
 
 // Clearing all the inputs
-function clearData(){
-    let allInputs = document.getElementsByClassName("data");
-    let allInputsArray = Array.prototype.slice.call(allInputs);
-    allInputsArray.forEach(element=>{
-        element.value="";
-    })
+function clearData() {
+  let allInputs = document.getElementsByClassName("data");
+  let allInputsArray = Array.prototype.slice.call(allInputs);
+  allInputsArray.forEach((element) => {
+    element.value = "";
+  });
 }
