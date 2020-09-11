c5b31d9426f7b95435b27fa6a7a24d8b8e905cc5
MaheshB77 <mbansode7696@gmail.com>
2020-09-11 20:22:19 +0530

    Added regular expression for "Contact No" validation

--- reactJS_assignments/assignments/src/assignment_3/components/pages/UserForm.js
+++ reactJS_assignments/assignments/src/assignment_3/components/pages/UserForm.js
@@ -16,7 +16,7 @@ export default class UserForm extends Component {
     );
   }
 }
-
+const mobileRegEx = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
 const StudentForm = () => {
   const formik = useFormik({
     initialValues: {
@@ -36,6 +36,7 @@ const StudentForm = () => {
     },
 
     // Validation using Yup
+
     validationSchema: Yup.object({
       firstName: Yup.string()
         .min(3, "Must be atleast 3 characters")
@@ -49,6 +50,7 @@ const StudentForm = () => {
         .email("Please enter valid email id")
         .required("Email is required"),
       userContact: Yup.string()
+        .matches(mobileRegEx, "Mobile number is not valid")
         .min(10, "Contact number must be of 10 digits")
         .max(10, "Contact number must be of 10 digits")
         .required("Contact number is required"),
